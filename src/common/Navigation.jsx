ca1bb5f8ce3b0d1dddd04091843df356a616a330
Alex0ne <alex0ne@fedora.kardzali.ddns.bulsat.com>
2019-11-06 00:31:29 +0200

    navigation bar styles added

--- src/common/Navigation.jsx
+++ src/common/Navigation.jsx
@@ -1,30 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthService } from '../services/Auth';
+import { ThemeContext } from 'styled-components';
+import { Navbar, NavbarBrand, Nav, NavItem, Button } from 'reactstrap';
+import { ReactComponent as LogoutIcon } from '../fonts/sign-out-alt-solid.svg';
 
 export default function Navigation() {
   const authService = new AuthService();
+  const themeContext = useContext(ThemeContext);
+  const theme = themeContext.styles;
+  const navbarStyles = {
+    backgroundColor: theme.appBarBackground,
+    borderColor: theme.appBarBorderColor
+  };
+  const navBarBrandStyles = {
+    fontFamily: 'SF Distant Galaxy',
+    cursor: 'pointer',
+    color: '#FFE300',
+    fontSize: 30
+  };
+  const navbarLinkStyles = {
+    textDecoration: 'none',
+    color: theme.appBarFontColor,
+    marginRight: 10,
+    lineHeight: 2
+  };
+  const logoutButtonStyles = {
+    borderColor: theme.appBarFontColor,
+    backgroundColor: 'none'
+  };
+  const logoutIconStyles = { width: 17, color: theme.appBarFontColor };
   return (
     <div>
-      <ul>
-        <li>
-          <p>SWAPP</p>
-        </li>
-        <li>
-          <Link to='/episodes'>episodes</Link>
-        </li>
-        <li>
-          <Link to='/characters'>characters</Link>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              authService.signout();
-            }}>
-            Sign out
-          </button>
-        </li>
-      </ul>
+      <Navbar style={navbarStyles} expand='md'>
+        <NavbarBrand
+          onClick={() => themeContext.swapTheme()}
+          style={navBarBrandStyles}>
+          SWAPP
+        </NavbarBrand>
+        <Nav className='ml-auto' navbar>
+          <NavItem>
+            <Link style={navbarLinkStyles} to='/episodes'>
+              episodes
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link style={navbarLinkStyles} to='/characters'>
+              characters
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Button
+              size='sm'
+              outline
+              style={logoutButtonStyles}
+              onClick={() => {
+                authService.signout();
+              }}>
+              <LogoutIcon style={logoutIconStyles} />
+            </Button>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
