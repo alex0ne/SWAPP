9c962d779997cd7101170caed66ae77e74b71de1
Alex0ne <alex0ne@fedora.kardzali.ddns.bulsat.com>
2019-11-09 17:49:20 +0200

    starships page UI updated

--- src/pages/Starship.jsx
+++ src/pages/Starship.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Api from '../services/Api';
+import { withTheme } from 'styled-components';
+import {
+  Card,
+  CardTitle,
+  CardImg,
+  CardText,
+  CardBody,
+  Row,
+  Col
+} from 'reactstrap';
 
-export default class Starship extends React.Component {
+class Starship extends React.Component {
   state = {
     starship: {}
   };
@@ -25,22 +35,93 @@ export default class Starship extends React.Component {
       image
     } = this.state.starship;
 
+    const theme = this.props.theme.styles;
+    const cardDescriptionValue = { color: theme.primaryHeadingFontColor };
+    const cardDescriptionKey = { color: theme.solidButtonBackground };
+
     return (
       <div>
-        <h2>{name}</h2>
-        <h3>({model})</h3>
-        <hr />
-        <section>
-          <h4>{name}</h4> <br />
-          <img width='250' src={image} alt={name} />
-          <p>Class: {starshipClass}</p>
-          <p>Cost: {cost}</p>
-          <p>Crew: {crew}</p>
-          <p>Max Atmospheric Speed: {maxAtmosphericSpeed}</p>
-          <p>Hyperdrive Rating: {hyperdriveRating}</p>
-        </section>
-        <section>Compare to Starship Class Max</section>
+        <div
+          style={{
+            padding: '1rem',
+            paddingTop: '3rem',
+            backgroundColor: theme.defaultBackground,
+            paddingBottom: '5rem',
+            color: theme.defaultFontColor
+          }}>
+          <div style={{ maxWidth: '800px', margin: 'auto' }}>
+            <h3
+              style={{
+                fontFamily: 'SF Distant Galaxy',
+                color: theme.primaryHeadingFontColor,
+                // marginBottom: '2rem'
+              }}>
+              {name}
+            </h3>
+            <h5  
+                style={{
+                fontFamily: 'SF Distant Galaxy',
+                color: theme.solidButtonBackground,
+                marginBottom: '2rem'
+              }}>({model})</h5>
+            <hr style={{ borderColor: 'black' }} />
+            <Row>
+              <Col md>
+                <Card
+                  style={{
+                    borderRadius: '8px',
+                    background: theme.cardBackground
+                  }}>
+                  <CardBody>
+                    <CardTitle
+                      style={{
+                        fontFamily: 'SF Distant Galaxy',
+                        color: theme.primaryHeadingFontColor
+                      }}>
+                      {name}
+                    </CardTitle>
+                    <CardImg src={image} alt={name} />
+                    <CardText
+                      style={{
+                        textAlign: 'left',
+                        marginTop: '1rem',
+                        color: ''
+                      }}>
+                      <span style={cardDescriptionKey}>Class:</span>{' '}
+                      <span style={cardDescriptionValue}>{starshipClass}</span>{' '}
+                      <br />
+                      <span style={cardDescriptionKey}>Cost:</span>{' '}
+                      <span style={cardDescriptionValue}> {cost}</span> <br />
+                      <span style={cardDescriptionKey}> Crew:</span>{' '}
+                      <span style={cardDescriptionValue}>{crew}</span> <br />
+                      <span style={cardDescriptionKey}>
+                        Max Atmospheric Speed:
+                      </span>{' '}
+                      <span style={cardDescriptionValue}>
+                        {' '}
+                        {maxAtmosphericSpeed}{' '}
+                      </span>{' '}
+                      <br />
+                      <span style={cardDescriptionKey}>
+                        Hyperdrive Rating:
+                      </span>{' '}
+                      <span style={cardDescriptionValue}>
+                        {' '}
+                        {hyperdriveRating}{' '}
+                      </span>{' '}
+                      <br />
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md>
+                <section>Compare to Starship Class Max</section>
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
     );
   }
 }
+export default withTheme(Starship);
